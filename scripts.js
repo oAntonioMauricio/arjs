cf6bb52056bf07ba5da5d2416d7ed4d8e02838f0
António Miguel Maurício <amiguema@emeal.nttdata.com>
2024-03-06 17:31:22 +0000

    add text infront of user

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ window.onload = () => {
     el.addEventListener("gps-camera-update-position", e => {
         if (!testEntityAdded) {
             alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
+
             // Add a box to the north of the initial GPS position
             const entity = document.createElement("a-box");
             entity.setAttribute("scale", {
@@ -21,11 +22,24 @@ window.onload = () => {
             document.querySelector("a-scene").appendChild(entity);
 
             // set lat and lang of text example
-            const textEl = document.getElementById('textTest');
-            textEl.setAttribute('gps-entity-place', {
-                latitude: e.detail.position.latitude,
+            const textEntity = document.createElement('a-text');
+
+            textEntity.setAttribute('value', "This will always face the user.");
+
+            textEntity.setAttribute('look-at', "[gps-new-camera]");
+
+            textEntity.setAttribute('scale', {
+                x: 75,
+                y: 75,
+                z: 75
+            });
+
+            textEntity.setAttribute('gps-new-entity-place', {
+                latitude: e.detail.position.latitude + 0.001,
                 longitude: e.detail.position.longitude
             })
+
+            document.querySelector("a-scene").appendChild(textEntity);
         }
         testEntityAdded = true;
     });
