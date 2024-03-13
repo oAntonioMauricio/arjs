7d179b0a4b5857ff402b890b5cb04c67c7802384
António Miguel Maurício <amiguema@emeal.nttdata.com>
2024-03-13 18:01:18 +0000

    only allow 1 element

--- tap-place.js
+++ tap-place.js
@@ -1,13 +1,21 @@
-// Copyright (c) 2021 8th Wall, Inc.
-/* globals AFRAME */
-
 // Component that places trees where the ground is clicked
 AFRAME.registerComponent('tap-place', {
     init() {
         const ground = document.getElementById('ground')
         ground.addEventListener('click', (event) => {
+
+            // remove old entites
+            let oldElement = document.getElementById('currentEl');
+            if (oldElement) {
+                oldElement.remove();
+            }
+
+
             // Create new entity for the new object
             const newElement = document.createElement('a-entity')
+
+            // give id
+            newElement.setAttribute('id', 'currentEl');
 
             // The raycaster gives a location of the touch in the scene
             const touchPoint = event.detail.intersection.point
