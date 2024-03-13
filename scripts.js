8c0376b67f2eac0ac75f893445de8315d8621c1e
António Miguel Maurício <amiguema@emeal.nttdata.com>
2024-03-13 12:08:02 +0000

    add fridge and reset view

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ window.onload = () => {
             alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
 
             // Add a box to the north of the initial GPS position
-            const entity = document.createElement("a-box");
+           /*  const entity = document.createElement("a-box");
             entity.setAttribute("scale", {
                 x: 20,
                 y: 20,
@@ -19,10 +19,10 @@ window.onload = () => {
                 latitude: e.detail.position.latitude + 0.001,
                 longitude: e.detail.position.longitude
             });
-            document.querySelector("a-scene").appendChild(entity);
+            document.querySelector("a-scene").appendChild(entity); */
 
             // set lat and lang of text example
-            const textEntity = document.createElement('a-text');
+            /* const textEntity = document.createElement('a-text');
 
             textEntity.setAttribute('value', "This will always face the user.");
 
@@ -39,12 +39,12 @@ window.onload = () => {
                 longitude: e.detail.position.longitude
             })
 
-            document.querySelector("a-scene").appendChild(textEntity);
+            document.querySelector("a-scene").appendChild(textEntity); */
 
             // model
             const modelEntity = document.createElement('a-entity');
 
-            modelEntity.setAttribute('gltf-model', './assets/laptop/laptop.glb');
+            modelEntity.setAttribute('gltf-model', './assets/fridge/kitchenFridge.glb');
 
             modelEntity.setAttribute('scale', {
                 x: 600,
@@ -75,6 +75,5 @@ const closeAr = () => {
 
 const resetView = () => {
     console.log("reseting view...");
-
-    
+    testEntityAdded = false;
 }
