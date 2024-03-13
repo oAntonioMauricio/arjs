e38e72fa17aaf1dbf2d1211b8630c3c16fe75bd4
António Miguel Maurício <amiguema@emeal.nttdata.com>
2024-03-13 15:49:07 +0000

    new cube teste

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
+window.onload = () => {
+    let testEntityAdded = false;
+    let modelEntity;
 
+    const el = document.querySelector("[gps-new-camera]");
+
+    el.addEventListener("gps-camera-update-position", e => {
+        if (!testEntityAdded) {
+            alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
+
+            // model
+            modelEntity = document.getElementById('myEnt');
+
+            modelEntity.setAttribute('gps-new-entity-place', {
+                latitude: e.detail.position.latitude + 0.001,
+                longitude: e.detail.position.longitude
+            })
+        }
+        testEntityAdded = true;
+    });
+};
