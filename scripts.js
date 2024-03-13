829e533157ab753e756bebac02764e8642199b6a
António Miguel Maurício <amiguema@emeal.nttdata.com>
2024-03-13 12:31:44 +0000

    add reset method

--- scripts.js
+++ scripts.js
@@ -1,55 +1,26 @@
 window.onload = () => {
     let testEntityAdded = false;
+    let modelEntity;
 
     const el = document.querySelector("[gps-new-camera]");
+
+    // enable controls
+    document.querySelector("a-camera").setAttribute("look-controls", "enabled", true);
+    document.querySelector("a-camera").setAttribute("wasd-controls", "enabled", true);
 
     el.addEventListener("gps-camera-update-position", e => {
         if (!testEntityAdded) {
             alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
 
-            // Add a box to the north of the initial GPS position
-           /*  const entity = document.createElement("a-box");
-            entity.setAttribute("scale", {
-                x: 20,
-                y: 20,
-                z: 20
-            });
-            entity.setAttribute('material', { color: 'red' });
-            entity.setAttribute('gps-new-entity-place', {
-                latitude: e.detail.position.latitude + 0.001,
-                longitude: e.detail.position.longitude
-            });
-            document.querySelector("a-scene").appendChild(entity); */
-
-            // set lat and lang of text example
-            /* const textEntity = document.createElement('a-text');
-
-            textEntity.setAttribute('value', "This will always face the user.");
-
-            textEntity.setAttribute('look-at', "[gps-new-camera]");
-
-            textEntity.setAttribute('scale', {
-                x: 75,
-                y: 75,
-                z: 75
-            });
-
-            textEntity.setAttribute('gps-new-entity-place', {
-                latitude: e.detail.position.latitude + 0.001,
-                longitude: e.detail.position.longitude
-            })
-
-            document.querySelector("a-scene").appendChild(textEntity); */
-
             // model
-            const modelEntity = document.createElement('a-entity');
+            modelEntity = document.createElement('a-entity');
 
             modelEntity.setAttribute('gltf-model', './assets/fridge/kitchenFridge.glb');
 
             modelEntity.setAttribute('scale', {
-                x: 600,
-                y: 600,
-                z: 600
+                x: 400,
+                y: 400,
+                z: 400
             });
 
             modelEntity.setAttribute('rotation', {
@@ -75,5 +46,8 @@ const closeAr = () => {
 
 const resetView = () => {
     console.log("reseting view...");
-    testEntityAdded = false;
+    // Disable the controls
+    document.querySelector("a-camera").setAttribute("look-controls", "enabled", false);
+    document.querySelector("a-camera").setAttribute("wasd-controls", "enabled", false);
 }
+
